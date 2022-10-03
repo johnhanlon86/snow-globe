53886ca54ad5a35baa79b9b68173f342d40c6bb4
John Hanlon <johnhanlon86@gmail.com>
2022-10-03 13:58:15 +0100

    Created rails app with models.

---
+++ db/migrate/20221003125629_create_tickets.rb
@@ -0,0 +1,13 @@
+class CreateTickets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tickets do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :performance, null: false, foreign_key: true
+      t.integer :status
+      t.integer :access
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
