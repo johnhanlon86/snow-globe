2cfcf81eae5813cf96de931e202c1dddb52ed63e
John Hanlon <johnhanlon86@gmail.com>
2022-11-09 15:33:24 +0000

    Updated Ticket model.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,13 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :performance
+  has_one :event, through: :performance
+  monetize :price_cents
+
+  enum status: {unsold: 0, waiting: 1}
+  enum access: {general: 0}
+
+  def places_in_cart_for(user)
+    update(status: :waiting, user: user)
+  end
 end
