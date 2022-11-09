83775167dd90146a06ef344e9457d0467db73876
John Hanlon <johnhanlon86@gmail.com>
2022-11-09 16:09:48 +0000

    Added AddsToCart helper/workfolw class.

---
+++ app/helpers/adds_to_cart.rb
@@ -0,0 +1,20 @@
+class AddsToCart
+
+    attr_accessor :user, :performance, :count, :success
+
+    def initialize(user:, performance:, count:)
+        @user = user
+        @performance = performance
+        @count = count.to_i
+        @success = false
+    end
+
+    def run
+        Ticket.transaction do
+            tickets = performance.unsold_tickets(count)
+            return if tickets.size != count
+            tickets.each { |ticket| ticket.places_in_cart_for(user) }
+            self.success = tickets.all?(&:valid?)
+            success
+    end
+end
