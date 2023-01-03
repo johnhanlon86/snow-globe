fd5154d044ec4d9e473b15aedc549f078d0bd8dd
John Hanlon <johnhanlon86@gmail.com>
2023-01-03 14:24:32 +0000

    Fix factories

--- spec/factories/performance_factory.rb
+++ spec/factories/performance_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :performance, class: "Performance" do
     association :event, factory: :event
     # user_id { 123 }
-    event_id { 456 }
+    # event_id { 456 }
     start_time {  }
     end_time {  }
   end
--- spec/factories/ticket_factory.rb
+++ spec/factories/ticket_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :ticket, class: "Ticket" do
     association :performance, factory: :performance
     # user_id { 123 }
-    performance_id { 456 }
+    # performance_id { 456 }
     access { 0 }
     status { 0 }
   end
