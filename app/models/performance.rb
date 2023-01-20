9b65f95ce0a13809a3140b89cb91ad9fe83a280f
John Hanlon <johnhanlon86@gmail.com>
2023-01-20 15:55:43 +0000

    Comment out method in performance model - needs fixing

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -2,7 +2,7 @@ class Performance < ApplicationRecord
   belongs_to :event
   has_many :tickets
 
-  def unsold_tickets(count)
-    tickets.where(status: "unsold").limit(count)
-  end
+  # def unsold_tickets(count)
+  #   tickets.where(status: "unsold").limit(count)
+  # end
 end
